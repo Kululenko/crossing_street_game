90f160494c14da84eafde38f5f7d8ab3f7c085a2
kululenko <alex.pmail@yahoo.com>
2024-03-09 08:16:34 +0100

    logic for collision with cars

--- car_manager.py
+++ car_manager.py
@@ -9,19 +9,18 @@ MOVE_INCREMENT = 10
 
 class CarManager(Turtle):
     def __init__(self):
-        super().__init__()
         self.carlist = []
 
-    def create_car(self): 
+    def create_car(self):     #wenn self statt new_car verwendet wird mache ich dann daraus ein Singleton? hatte ein komischen Bug wo die autos nicht kamen. Nur einer zur Zeit 
         random_int = random.randint(1,6)
         if random_int == 1:
-            self.shape("square")        #bug with square?
-            self.color(random.choice(COLORS))
-            self.penup()
-            self.goto(y=random.randint(-250,250),x=290)
-            self.shapesize(stretch_len=2)
-            self.setheading(180)
-            self.carlist.append(self)
+            new_car = Turtle("square")        
+            new_car.color(random.choice(COLORS))
+            new_car.penup()
+            new_car.goto(y=random.randint(-250,250),x=300)
+            new_car.shapesize(stretch_len=2)
+            new_car.setheading(180)
+            self.carlist.append(new_car)
         
         
 
--- main.py
+++ main.py
@@ -20,7 +20,11 @@ screen.onkey(player.move_up,"Up")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.2)
+    time.sleep(0.1)
     screen.update()
     car_manager.create_car()
     car_manager.movement()
+    for car in car_manager.carlist:
+        if player.distance(car) < 20:
+            pass
+            
