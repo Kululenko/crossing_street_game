b4070402b46f4e8f820ac24c0ceaaa5d3e4c3a65
kululenko <alex.pmail@yahoo.com>
2024-03-23 20:51:51 +0100

    fixed a bug

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+
+#Setup and objects
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
@@ -15,10 +17,12 @@ scoreboard = Scoreboard()
 
 
 
-
+#Movement
 screen.listen()
 screen.onkey(player.move_up,"Up")
 
+
+#logic and rules
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
